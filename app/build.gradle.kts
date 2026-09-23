6e8bc3b18d9740a6ff759924b3b688f2d2875efa
RUBEN MV <rubenmuquillazav@gmail.com>
2026-09-23 01:16:00 -0500

    Resolucion Preg 2 y Preg 6
    Soluciones de la preg2 y preg 6 en la T1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,9 +25,13 @@ android {
             }
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+    viewBinding{
+        enable=true
     }
 }
 
